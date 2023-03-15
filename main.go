992e31aa49f970c8d2eaab5c32c7682b8cf8490a
Oleksandr Novak <onovak@spscommerce.com>
2023-03-15 10:50:51 +0100

    refactoring

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"runtime/debug"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -53,7 +52,7 @@ type Node struct {
 	Pods []PodRef `json:"pods"`
 }
 
-func ParseInputArguments() (*RunConfiguration, error) {
+func parseInputArguments() (*RunConfiguration, error) {
 
 	result := RunConfiguration{
 		KubeConfig:      "",
@@ -67,7 +66,7 @@ func ParseInputArguments() (*RunConfiguration, error) {
 		flag.StringVar(&result.KubeConfig, "kubeconfig", "", "absolute path to the kubeconfig file")
 	}
 	flag.IntVar(&result.Port, "port", result.Port, fmt.Sprintf("(optional) port on which app would expose metrics, Defaults to %v", result.Port))
-	flag.IntVar(&result.RefreshInterval, "refresh-interval", result.RefreshInterval, fmt.Sprintf("(optional) fefresh interval to re-read the metrics values, Defaults to %v", result.RefreshInterval))
+	flag.IntVar(&result.RefreshInterval, "refresh-interval", result.RefreshInterval, fmt.Sprintf("(optional) refresh interval (in seconds) to re-read the metrics values, Defaults to %v", result.RefreshInterval))
 	flag.BoolVar(&result.PlainLogs, "plain-logs", result.PlainLogs, fmt.Sprintf("(optional) port on which app would expose metrics, Defaults to %v", result.Port))
 	flag.Parse()
 	return &result, nil
@@ -81,7 +80,7 @@ func getLogEncoder(usePlain bool, config zapcore.EncoderConfig) zapcore.Encoder 
 	return zapcore.NewJSONEncoder(config)
 }
 
-func UpEndpointHandler(w http.ResponseWriter, r *http.Request) {
+func upEndpointHandler(w http.ResponseWriter, r *http.Request) {
 	_, err := w.Write([]byte("up"))
 	if err != nil {
 		zap.Error(fmt.Errorf("unable to handle request to up endpoint: %s", err))
@@ -89,8 +88,7 @@ func UpEndpointHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func metricsProcessing(ctx context.Context, clientset *kubernetes.Clientset, metric *prometheus.GaugeVec, nodeName string, logger *zap.SugaredLogger, runtimeConfig *RunConfiguration) {
-	var pods *[]Pod
-	var err error
+	pods := &[]Pod{}
 	for {
 		select {
 		// Cancel goroutine after node is deleted from a cluster
@@ -99,29 +97,26 @@ func metricsProcessing(ctx context.Context, clientset *kubernetes.Clientset, met
 			return
 		default:
 			logger.Infof("Gathering metrics for pods on node: %v", nodeName)
-			if pods == nil {
-				pods, err = processSingleNodeMetrics(ctx, clientset, logger, metric, nodeName)
-				if err != nil {
-					logger.Errorf("Gathering metrics for pods on node: %v failed", nodeName)
-				}
-			} else {
-				newPods, err := processSingleNodeMetrics(ctx, clientset, logger.Named("processSingleNodeMetrics"), metric, nodeName)
-				if err != nil {
-					logger.Errorf("Processing metrics for node %v failed with error: %s", nodeName, err)
-				}
-				//Deleting metrics of pods that do not exist anymore
-				newPodsNameMap := make(map[string]bool)
-				for _, pod := range *newPods {
-					newPodsNameMap[pod.PodName] = true
-				}
-				for _, pod := range *pods {
-					if _, ok := newPodsNameMap[pod.PodName]; !ok {
-						metric.DeletePartialMatch(prometheus.Labels{"pod": pod.PodName, "node": nodeName})
-						logger.Infof("Pod %v was deleted", pod.PodName)
-					}
-				}
-				pods = newPods
+
+			newPods, err := processSingleNodeMetrics(ctx, clientset, logger, metric, nodeName)
+			if err != nil {
+				logger.Errorf("unable to process node metrics: %s", err)
+				newPods = &[]Pod{}
 			}
+
+			//Deleting metrics of pods that do not exist anymore
+			newPodsNameMap := make(map[string]bool)
+			for _, pod := range *newPods {
+				newPodsNameMap[pod.PodName] = true
+			}
+			for _, pod := range *pods {
+				if _, ok := newPodsNameMap[pod.PodName]; !ok {
+					metric.DeletePartialMatch(prometheus.Labels{"pod": pod.PodName, "node": nodeName})
+					logger.Infof("Pod %v was deleted", pod.PodName)
+				}
+			}
+			pods = newPods
+
 			time.Sleep(time.Duration(runtimeConfig.RefreshInterval) * time.Second)
 		}
 
@@ -133,31 +128,33 @@ func createNodeInformer(ctx context.Context,
 	logger *zap.SugaredLogger, runtimeConfig *RunConfiguration) {
 
 	informerFactory := informers.NewSharedInformerFactoryWithOptions(client, time.Duration(time.Duration.Seconds(60)))
-	nodeinformer := informerFactory.Core().V1().Nodes().Informer()
+	nodeInformer := informerFactory.Core().V1().Nodes().Informer()
 
 	go informerFactory.Start(ctx.Done())
 
-	if !cache.WaitForCacheSync(ctx.Done(), nodeinformer.HasSynced) {
+	if !cache.WaitForCacheSync(ctx.Done(), nodeInformer.HasSynced) {
 		logger.Fatalf("timed out waiting for caches to sync")
 	}
 
-	nodesContextMap := make(map[string]context.CancelFunc)
-	_, err := nodeinformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+	nodeContextCancellationFuncs := make(map[string]context.CancelFunc)
+	_, err := nodeInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
 			node := obj.(*v1.Node)
 			logger.Infof("Node was added to the cluster : %v", node.Name)
 			metricsProcessingCtx, cancel := context.WithCancel(ctx)
 			// Saving context to the map for node deleting event handling
-			nodesContextMap[node.Name] = cancel
+			nodeContextCancellationFuncs[node.Name] = cancel
 			// Starting goroutine for per node metrics monitoring
-			go metricsProcessing(metricsProcessingCtx, client, metric, node.Name, logger.Named("metricsProcessing").With("nodeName", node.Name), runtimeConfig)
+			go metricsProcessing(metricsProcessingCtx, client, metric, node.Name,
+				logger.Named("metricsProcessing").With("nodeName", node.Name),
+				runtimeConfig)
 
 		},
 
 		DeleteFunc: func(obj interface{}) {
 			node := obj.(*v1.Node)
-			nodesContextMap[node.Name]()
-			delete(nodesContextMap, node.Name)
+			nodeContextCancellationFuncs[node.Name]()
+			delete(nodeContextCancellationFuncs, node.Name)
 			logger.Infof("Node %v was deleted", node.Name)
 
 		},
@@ -176,26 +173,20 @@ func registerPrometheusMetrics(ctx context.Context) *prometheus.GaugeVec {
 	return result
 }
 
-// Gets Node statitics from API, parsing it and register prometheus metric for each pod on the node
+// Gets Node statistics from API, parsing it and register prometheus metric for each pod on the node
 func processSingleNodeMetrics(ctx context.Context, clientset *kubernetes.Clientset, logger *zap.SugaredLogger, metric *prometheus.GaugeVec, nodeName string) (*[]Pod, error) {
-	defer func() {
-		err := recover()
-		if err != nil {
-			logger.Error(err.(error), fmt.Sprintf("PANIC at %s", debug.Stack()))
-		}
-	}()
-
-	pods := []Pod{}
-	node := Node{}
-
 	response, err := clientset.CoreV1().RESTClient().Get().Resource("nodes").Name(nodeName).SubResource("proxy").Suffix("stats/summary").DoRaw(ctx)
 	if err != nil {
-		logger.Errorf("Error occured when reaching kubelet api for getting metrics of node %v", nodeName)
-
+		return nil, fmt.Errorf("error occured when reaching kubelet api for getting node metrics: %s", err)
 	}
 
-	json.Unmarshal(response, &node)
+	node := Node{}
+	err = json.Unmarshal(response, &node)
+	if err != nil {
+		return nil, fmt.Errorf("unexpected response from kube api: %s", err)
+	}
 
+	pods := []Pod{}
 	for _, pod := range node.Pods {
 		if _, ok := pod.EphemeralStorage.Usedbytes.(float64); ok {
 			metric.With(prometheus.Labels{"pod": pod.PodRef.Name, "node": nodeName, "namespace": pod.PodRef.Namespace}).Set(pod.EphemeralStorage.Usedbytes.(float64))
@@ -207,7 +198,7 @@ func processSingleNodeMetrics(ctx context.Context, clientset *kubernetes.Clients
 
 func main() {
 	// Parsing input arguments
-	runtimeConfig, err := ParseInputArguments()
+	runtimeConfig, err := parseInputArguments()
 	if err != nil {
 		panic(err)
 	}
@@ -258,16 +249,15 @@ func main() {
 	}
 
 	rootCtx := context.Background()
-	ctx, _ := context.WithCancel(rootCtx)
 
 	// Setting prometheus metric
-	metric := registerPrometheusMetrics(ctx)
+	metrics := registerPrometheusMetrics(rootCtx)
 
 	// Starting goroutine with main logic
-	go createNodeInformer(ctx, clientset, metric, logger.Named("nodeInformer"), runtimeConfig)
+	go createNodeInformer(rootCtx, clientset, metrics, logger.Named("nodeInformer"), runtimeConfig)
 	// Set up http endpoints
 	http.Handle("/metrics", promhttp.Handler())
-	http.Handle("/up", http.HandlerFunc(UpEndpointHandler))
+	http.Handle("/up", http.HandlerFunc(upEndpointHandler))
 	if http.ListenAndServe(fmt.Sprintf(":%d", runtimeConfig.Port), nil) != nil {
 		logger.Fatalf("unable to listen port %d: %s", runtimeConfig.Port, err)
 	}
