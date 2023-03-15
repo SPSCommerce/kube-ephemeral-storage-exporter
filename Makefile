e1a13ce8f790502fed3018b4ae689b1bb4615b5b
Andriy Vasylyna <avasylyna@spscommerce.com>
2023-03-15 09:29:15 +0200

    review fixes

--- Makefile
+++ Makefile
@@ -4,13 +4,12 @@
 # The version that will be used in docker tags
 VERSION ?= $(shell git rev-parse --short HEAD)
 
-ENTRY_POINT=./cmd
 
 image: build_ci # Build executable and docker image
 	docker build -t kube-hpa-scale-to-zero:$(VERSION) .
 
 local: # Run go application locally
-	go run ${ENTRY_POINT}
+	go run main.go
 
 run: image # Run docker container in foreground
 	docker run -p 8080:8080 kube-hpa-scale-to-zero:$(VERSION)
--- main.go
+++ main.go
@@ -1,20 +1,92 @@
-package internal
+package main
 
 import (
 	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
 	"runtime/debug"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
-	"go.uber.org/zap"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/informers"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 )
+
+type RunConfiguration struct {
+	KubeConfig      string
+	Port            int
+	RefreshInterval int
+	PlainLogs       bool
+}
+
+type Pod struct {
+	PodName string
+}
+
+type PodRef struct {
+	PodRef struct {
+		Name      string `json:"name"`
+		Namespace string `json:"namespace"`
+	} `json:"podRef"`
+	EphemeralStorage struct {
+		Usedbytes any `json:"usedBytes"`
+	} `json:"ephemeral-storage"`
+}
+
+type Node struct {
+	Node struct {
+		NodeName string `json:"nodeName"`
+	} `json:"node"`
+	Pods []PodRef `json:"pods"`
+}
+
+func ParseInputArguments() (*RunConfiguration, error) {
+
+	result := RunConfiguration{
+		KubeConfig:      "",
+		Port:            9000,
+		PlainLogs:       false,
+		RefreshInterval: 60,
+	}
+	if home := homedir.HomeDir(); home != "" {
+		flag.StringVar(&result.KubeConfig, "kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		flag.StringVar(&result.KubeConfig, "kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.IntVar(&result.Port, "port", result.Port, fmt.Sprintf("(optional) port on which app would expose metrics, Defaults to %v", result.Port))
+	flag.IntVar(&result.RefreshInterval, "refresh-interval", result.RefreshInterval, fmt.Sprintf("(optional) fefresh interval to re-read the metrics values, Defaults to %v", result.RefreshInterval))
+	flag.BoolVar(&result.PlainLogs, "plain-logs", result.PlainLogs, fmt.Sprintf("(optional) port on which app would expose metrics, Defaults to %v", result.Port))
+	flag.Parse()
+	return &result, nil
+
+}
+
+func getLogEncoder(usePlain bool, config zapcore.EncoderConfig) zapcore.Encoder {
+	if usePlain {
+		return zapcore.NewConsoleEncoder(config)
+	}
+	return zapcore.NewJSONEncoder(config)
+}
+
+func UpEndpointHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("up"))
+	if err != nil {
+		zap.Error(fmt.Errorf("unable to handle request to up endpoint: %s", err))
+	}
+}
 
 func metricsProcessing(ctx context.Context, clientset *kubernetes.Clientset, metric *prometheus.GaugeVec, nodeName string, logger *zap.SugaredLogger, runtimeConfig *RunConfiguration) {
 	var pods *[]Pod
@@ -125,8 +197,79 @@ func processSingleNodeMetrics(ctx context.Context, clientset *kubernetes.Clients
 	json.Unmarshal(response, &node)
 
 	for _, pod := range node.Pods {
-		metric.With(prometheus.Labels{"pod": pod.PodRef.Name, "node": nodeName, "namespace": pod.PodRef.Namespace}).Set(pod.EphemeralStorage.Usedbytes.(float64))
+		if _, ok := pod.EphemeralStorage.Usedbytes.(float64); ok {
+			metric.With(prometheus.Labels{"pod": pod.PodRef.Name, "node": nodeName, "namespace": pod.PodRef.Namespace}).Set(pod.EphemeralStorage.Usedbytes.(float64))
+		}
 		pods = append(pods, Pod{PodName: pod.PodRef.Name})
 	}
 	return &pods, nil
+}
+
+func main() {
+	// Parsing input arguments
+	runtimeConfig, err := ParseInputArguments()
+	if err != nil {
+		panic(err)
+	}
+
+	// Setting Logger
+	infoLevel := zap.LevelEnablerFunc(func(level zapcore.Level) bool {
+		return level <= zapcore.WarnLevel
+	})
+
+	errorLevel := zap.LevelEnablerFunc(func(level zapcore.Level) bool {
+		return level > zapcore.WarnLevel
+	})
+
+	stdoutSyncer := zapcore.Lock(os.Stdout)
+	stderrSyncer := zapcore.Lock(os.Stderr)
+
+	encoderCfg := zap.NewProductionEncoderConfig()
+	encoderCfg.TimeKey = "timestamp"
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	core := zapcore.NewTee(
+		zapcore.NewCore(
+			getLogEncoder(runtimeConfig.PlainLogs, encoderCfg),
+			stdoutSyncer,
+			infoLevel,
+		),
+		zapcore.NewCore(
+			getLogEncoder(runtimeConfig.PlainLogs, encoderCfg),
+			stderrSyncer,
+			errorLevel,
+		),
+	)
+
+	logger := zap.New(core).Named("Root").Sugar()
+	defer logger.Sync()
+
+	logger.Infof("Starting with configuration %#v", runtimeConfig)
+
+	// Building kubernetes clientset
+	config, _ := clientcmd.BuildConfigFromFlags("", runtimeConfig.KubeConfig)
+	if err != nil {
+		logger.Fatalf("Unable to build config: %s", err)
+	}
+
+	clientset := kubernetes.NewForConfigOrDie(config)
+	if err != nil {
+		logger.Fatalf("Unable to create kubernetes clientset: %s", err)
+	}
+
+	rootCtx := context.Background()
+	ctx, _ := context.WithCancel(rootCtx)
+
+	// Setting prometheus metric
+	metric := registerPrometheusMetrics(ctx)
+
+	// Starting goroutine with main logic
+	go createNodeInformer(ctx, clientset, metric, logger.Named("nodeInformer"), runtimeConfig)
+	// Set up http endpoints
+	http.Handle("/metrics", promhttp.Handler())
+	http.Handle("/up", http.HandlerFunc(UpEndpointHandler))
+	if http.ListenAndServe(fmt.Sprintf(":%d", runtimeConfig.Port), nil) != nil {
+		logger.Fatalf("unable to listen port %d: %s", runtimeConfig.Port, err)
+	}
+
 }
